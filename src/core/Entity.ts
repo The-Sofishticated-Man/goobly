8fe40b59c887c0a67c218db7265310455d5d6231
islam <lameche1islam@gmail.com>
2026-02-03 18:01:44 +0100

    feat: add SVG support and refactor Entity class for visual handling

--- src/core/Entity.ts
+++ src/core/Entity.ts
@@ -2,6 +2,7 @@ import {
   Assets,
   Container,
   FederatedPointerEvent,
+  Graphics,
   Sprite,
   Texture,
   Ticker,
@@ -14,11 +15,20 @@ interface EntityOptions {
 }
 
 export abstract class Entity {
+  protected abstract defaultTexturePath: string;
+
+  //global container that holds everything
   public container = new Container();
-  public sprite!: Sprite;
+
+  //container that holds the actual image of the entity
+  public visual!: Container;
+
   protected isDragging = false;
   protected options: EntityOptions;
+
+  //reference to the experiment the entity is part of
   protected experiment!: BaseExperiment;
+
   constructor(options: EntityOptions = {}) {
     this.options = options;
   }
@@ -26,59 +36,80 @@ export abstract class Entity {
   async init(experiment: BaseExperiment): Promise<this> {
     this.experiment = experiment;
     const texPath = this.options.texturePath ?? this.defaultTexturePath;
-    const tex = await Assets.load<Texture>(texPath);
-    this.sprite = new Sprite(tex);
-    this.sprite.anchor.set(0.5);
-    this.sprite.eventMode = "static";
+    this.visual = await this.createVisual(texPath);
 
-    this.container.addChild(this.sprite);
+    this.container.addChild(this.visual);
 
     if (this.options.draggable) this.enableDragging();
     return this;
   }
 
   public enableDragging() {
-    this.sprite.eventMode = "static";
-    this.sprite.cursor = "pointer";
-    this.sprite.interactive = true;
+    this.visual.cursor = "pointer";
+    this.visual.interactive = true;
 
     const dragOffset = { x: 0, y: 0 };
 
-    this.sprite.on("pointerdown", (e: FederatedPointerEvent) => {
+    this.visual.on("pointerdown", (e: FederatedPointerEvent) => {
       this.isDragging = true;
 
       const pos = e.global;
-      dragOffset.x = pos.x - this.sprite.x;
-      dragOffset.y = pos.y - this.sprite.y;
+      dragOffset.x = pos.x - this.visual.x;
+      dragOffset.y = pos.y - this.visual.y;
 
-      this.sprite.alpha = 0.8; // Optional feedback
+      this.visual.alpha = 0.8;
     });
 
-    this.sprite.on("pointerup", () => {
+    this.visual.on("pointerup", () => {
       this.isDragging = false;
-      this.sprite.alpha = 1; // Optional feedback
+      this.visual.alpha = 1;
     });
 
-    this.sprite.on("pointerupoutside", () => {
+    this.visual.on("pointerupoutside", () => {
       this.isDragging = false;
-      this.sprite.alpha = 1;
+      this.visual.alpha = 1;
     });
 
-    this.sprite.on("pointermove", (e: FederatedPointerEvent) => {
+    this.visual.on("pointermove", (e: FederatedPointerEvent) => {
       if (!this.isDragging) return;
       const pos = e.global;
-      this.sprite.x = pos.x - dragOffset.x;
-      this.sprite.y = pos.y - dragOffset.y;
+      this.visual.x = pos.x - dragOffset.x;
+      this.visual.y = pos.y - dragOffset.y;
     });
 
     return this;
   }
+  private async createVisual(path: string): Promise<Container> {
+    // handle svg file
+    if (path.endsWith(".svg")) {
+      const svgContext = await Assets.load({
+        src: path,
+        data: { parseAsGraphicsContext: true },
+      });
+      const graphics = new Graphics(svgContext);
 
-  abstract start(): void;
-  abstract update(ticker: Ticker): void;
+      const bounds = graphics.getLocalBounds();
+      graphics.pivot.set(
+        bounds.x + bounds.width / 2,
+        bounds.y + bounds.height / 2,
+      );
+
+      return graphics;
+    } else {
+      // handle other file types (jpg, png, etc...)
+      const tex = await Assets.load<Texture>(path);
+      const sprite = new Sprite(tex);
+      sprite.anchor.set(0.5);
+      return sprite;
+    }
+  }
+  // cleanup resources
   destroy(): void {
     this.container.destroy({ children: true });
   }
 
-  protected abstract defaultTexturePath: string;
+  // runs once at the beginning
+  abstract start(): void;
+  // runs on every frame
+  abstract update(ticker: Ticker): void;
 }
