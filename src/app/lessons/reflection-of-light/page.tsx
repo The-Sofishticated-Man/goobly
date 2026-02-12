61ee0a207a3a2e112cf56699c621a21a73bf4192
Naioua d Anas <itsanssoff@gmail.com>
2026-02-12 10:20:51 +0100

    add logo i think

--- src/app/lessons/reflection-of-light/page.tsx
+++ src/app/lessons/reflection-of-light/page.tsx
@@ -7,18 +7,6 @@ import Playground from "@/components/Playground";
 import Image from 'next/image';
 import Math from '@/components/Math';
 
-const markdown = `
-## Reflection of Light
-
-When a ray of light strikes a mirror, the **angle of incidence** equals the **angle of reflection**.
-
-$$
-\\theta_i = \\theta_{re}
-$$
-
-Below is an interactive visualization.
-`;
-
 export default function ReflectionLesson() {
   return (
     <main className="mx-auto space-y-8 p-8">
