2090eafd4dd16856058a737d61de4d1ba5501887
islam <lameche1islam@gmail.com>
2026-02-20 15:43:47 +0100

    update Playground component dimensions for improved layout

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -4,9 +4,9 @@ import { Layer, Rect, Stage } from "react-konva";
 
 export default function Playground({ module }: { module: string }) {
   return (
-    <Stage width={500} height={300} background={"#f0f0f0"}>
+    <Stage width={1800} height={800} background={"#f0f0f0"}>
       <Layer>
-        <Rect width={500} height={300} fill="white" />
+        <Rect width={1800} height={800} stroke={"#f0f0f0"} />
         <LaserPointer />
       </Layer>
     </Stage>
